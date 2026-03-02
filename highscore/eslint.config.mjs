636e35c897a59a9340b722352490a7c14d49fb11
zarin.vansteelandt <zarin.vansteelandt@easypost.eu>
2026-03-02 09:01:03 +0100

    added 404 and removed valentine theme

--- highscore/eslint.config.mjs
+++ highscore/eslint.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+
+const nextVitals = require('eslint-config-next/core-web-vitals')
+const nextTs = require('eslint-config-next/typescript')
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +12,11 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
